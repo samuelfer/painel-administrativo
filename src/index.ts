67383171a304b465567bcdfc7a84788f2503b5ab
samuelfer <samuelfesant@gmail.com>
2023-01-06 20:03:59 -0300

    Add routes para crud de produtos

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 import "./connection";
+import ProductController from "./controllers/ProductController";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+app.use(bodyParser.json());
+
+app.get("/api/products", ProductController.getAll);
+app.get("/api/products/:id", ProductController.getById);
+app.post("/api/products", ProductController.create);
+app.put("/api/products/:id", ProductController.update);
+app.delete("/api/products/:id", ProductController.delete);
 
 app.get("/", (req, resp) => {
     resp.send("Server up");
