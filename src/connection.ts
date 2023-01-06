8622f2154ddf70942b23fa121058d97c1f119660
samuelfer <samuelfesant@gmail.com>
2023-01-06 16:58:38 -0300

    Add conexão com a base de dados

---
+++ src/connection.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "painel_admin",
+    synchronize: true,
+    logging: true,
+    entities: [__dirname + "/**/*.entity.ts"],
+    subscribers: [],
+    migrations: [],
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Connected successfull");
+    })
+    .catch((error) => console.log(error));
