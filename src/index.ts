e3ac3de9da066dffb06675519ba314af3c41c69b
samuelfer <samuelfesant@gmail.com>
2023-01-06 16:59:19 -0300

    Config inicial do servidor

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get("/", (req, resp) => {
+    resp.send("Server up");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server runnig in port ${PORT}`);
+});
